a9cbacc3d1b3f41302d38d09962de7fe8b52d470
Natnael Adane <natnaeladane21@gmail.com>
2024-03-05 09:35:50 +0300

    changes

--- src/screens/home/widgets/forms/Department_service.tsx
+++ src/screens/home/widgets/forms/Department_service.tsx
@@ -4,13 +4,17 @@ import { Department } from '../../../../models/department/department'
 import { BookFilledIcon } from '../../../../widgets/icons/icons'
 import { Collapse, Divider, Select } from 'antd'
 import './Department.css'
+import axios from 'axios'
 const Department_service = forwardRef((props:{onSubmit:SubmitHandler<any>,departments:Department[]}, ref: React.ForwardedRef<any>) => {
 
     const [options,setOptions] = useState()
     const [selectedDepartment,setSelectedDepartment] = useState([])
+    const [services,setServices] = useState([])
+    const [categories,setCategories] = useState([])
+    const [data,setData] = useState([])
     console.log("======++++++++++",props.departments)
     useEffect(()=>{
-           const x:any =  props.departments.map(d=>({...d,value:JSON.stringify(d),label:d["name"]+" - "+d["LicenceNumber"]}))
+           const x:any =  props.departments.map(d=>({...d,value:JSON.stringify(d),label:d["name"]+" - "+ d["LicenceNumber"]}))
            setOptions(x)
     },[])
 
@@ -22,8 +26,45 @@ const Department_service = forwardRef((props:{onSubmit:SubmitHandler<any>,depart
 
         }
     }))
-    const handleChange = (value: string[]) => {
-        console.log(`selected ${value.length}`);
+    const handleChange = (value: string[],d:any) => {
+        // console.log(`selected 33 ${value.length}`);
+        const val = value.map(v=>JSON.parse(v))
+
+        const cat = val.map(async(dep)=>{
+          const len = dep["services"].length -1
+          if(len<0){
+
+            return {}
+          }else{
+            const res = await axios.get(`/orgs/etrade_code?code=${dep["services"][len]["Code"]}`)
+            setCategories([...categories,res.data["data"]])
+            return res.data["data"]
+          }
+          
+
+
+        })
+        console.log("cat",categories)
+        
+        // setSelectedDepartment(value.map(v=>JSON.parse(v)))
+        // setData(value.map(v=>JSON.stringify(v)))
+      };
+      const serviceHandleChange = (value: string[]) => {
+        
+        // console.log(`selected ${value.length}`);
+        // console.log(`selected ${value}`);
+       const data = value.map(v=>JSON.parse(v))
+        const id = data[0].id
+        const newData = data.map(d=>{
+          console.log("id",id)
+          // const service = value.
+          // if(d["LicenceNumber"])
+        })
+        
+        
+        
+        
+        
       };
   return (
  
@@ -57,25 +98,25 @@ const Department_service = forwardRef((props:{onSubmit:SubmitHandler<any>,depart
                            Selected Departments
                         </div>
           
-    <Collapse
+    {/* <Collapse
     accordion
-    onChange={(v)=>console.log("collapse",v)}
+    onChange={(v)=>{
+        const dep:any = selectedDepartment.find(d=>d["LicenceNumber"]==v[0])
+        console.log('dep',dep.services)
+        if(dep["services"]){
+
+        
+        const x:any =  dep["services"].map(s=>({value:JSON.stringify({data:s,id:v[0]}),label:s["Name"]+" - "+ s["Code"]}))
+
+        setServices(x)
+        }else{
+          setServices([])
+        }
+
+    }}
     
       
-      items={[{ key: '2', label: 'This is default size panel header', children: <div>
-        <Select
-       className='department_select'
-      mode="multiple"
-      allowClear
-      style={{ width: '100%' }}
-      placeholder="Please select"
-      defaultValue={[]}
-      onChange={handleChange}
-      options={options}
-    />
-
-      </div> },
-      { key: 'ww', label: 'This is default size panel header', children: <div>
+      items={selectedDepartment.map(dep=>({key:dep["LicenceNumber"],label:dep["name"],children:<div>
       <Select
      className='department_select'
     mode="multiple"
@@ -83,12 +124,13 @@ const Department_service = forwardRef((props:{onSubmit:SubmitHandler<any>,depart
     style={{ width: '100%' }}
     placeholder="Please select"
     defaultValue={[]}
-    onChange={handleChange}
-    options={options}
+    onChange={serviceHandleChange}
+    options={services}
   />
 
-    </div> }]}
-    />    
+    </div>}))
+     }
+    />     */}
 
 
 
